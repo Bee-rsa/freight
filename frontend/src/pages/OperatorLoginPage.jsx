ec6d2e0b9cb484ae6ffe62019703c26fbe0d891b
Brendan <cleavermedia@outlook.com>
2024-11-16 12:57:20 +0200

    New Updated Version

--- frontend/src/pages/OperatorLoginPage.jsx
+++ frontend/src/pages/OperatorLoginPage.jsx
@@ -6,87 +6,101 @@ import Input from "../components/Input";
 import { useAuthsStore } from "../store/authsStore";
 
 const OperatorLoginPage = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate(); // Step 1: Get the navigate function
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	const { operatorLogin, isLoading, error } = useAuthsStore();
+  const { operatorLogin, isLoading, error } = useAuthsStore();
 
-	const handleOperatorLogin = async (e) => {
-		e.preventDefault();
-		const success = await operatorLogin(email, password);
-		if (success) {
-			navigate("/overview");
-		} else {
-			// Handle login failure, e.g., show an error message
-		}
-	};
+  const handleOperatorLogin = async (e) => {
+    e.preventDefault();
+    const success = await operatorLogin(email, password);
+    if (success) {
+      navigate("/overview");
+    } else {
+      // Handle login failure, e.g., show an error message
+    }
+  };
 
-	return (
-		<div
-		  className="min-h-screen w-full flex justify-center items-center"
-		  style={{
-			backgroundImage: "linear-gradient(135deg, #1B2E52 0%, #5185AB 100%)", // Gradient background
-		  }}
-		>
-		  <motion.div
-			initial={{ opacity: 0, y: 20 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-			className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
-		  >
-			<div className="p-8 sm:p-6">
-			  <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-700 to-blue-900 text-transparent bg-clip-text">
-				Welcome Back
-			  </h2>
-	
-			  <form onSubmit={handleOperatorLogin}>
-				<Input
-				  icon={Mail}
-				  type="email"
-				  placeholder="Email Address"
-				  value={email}
-				  onChange={(e) => setEmail(e.target.value)}
-				/>
-	
-				<Input
-				  icon={Lock}
-				  type="password"
-				  placeholder="Password"
-				  value={password}
-				  onChange={(e) => setPassword(e.target.value)}
-				/>
-	
-				<div className="flex items-center mb-6">
-				  <Link to="/forgot-password" className="text-sm text-blue-400 hover:underline">
-					Forgot password?
-				  </Link>
-				</div>
-				{error && <p className="text-red-500 font-semibold mb-2">{error}</p>}
-	
-				<motion.button
-				  whileHover={{ scale: 1.02 }}
-				  whileTap={{ scale: 0.98 }}
-				  className="w-full py-3 px-4 bg-gradient-to-r from-blue-700 to-custom-blue text-white font-bold rounded-lg shadow-lg hover:from-blue-600 hover:to-custom-blue focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
-				  type="submit"
-				  disabled={isLoading}
-				>
-				  {isLoading ? <Loader className="w-6 h-6 animate-spin mx-auto" /> : "Login"}
-				</motion.button>
-			  </form>
-			</div>
-			<div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
-			  <p className="text-sm text-gray-400">
-				Don&apos;t have an account?{" "}
-				<Link to="/operator-signup" className="text-blue-400 hover:underline">
-				  Sign up
-				</Link>
-			  </p>
-			</div>
-		  </motion.div>
-		</div>
-	  );
-	};
-	
-	export default OperatorLoginPage;
-	
+  return (
+    <div
+      className="min-h-screen w-full flex justify-center items-center"
+      style={{
+        backgroundImage: "linear-gradient(135deg, #1B2E52 0%, #5185AB 100%)",
+      }}
+    >
+      <style>
+        {`
+          /* Mobile Styles */
+          @media only screen and (max-width: 768px) {
+            .max-w-md {
+              margin: 20px;
+            }
+
+            .p-8 {
+              padding: 20px;
+            }
+          }
+        `}
+      </style>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+      >
+        <div className="p-8 sm:p-6">
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-700 to-blue-900 text-transparent bg-clip-text">
+            Welcome Back
+          </h2>
+
+          <form onSubmit={handleOperatorLogin}>
+            <Input
+              icon={Mail}
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              icon={Lock}
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="flex items-center mb-6">
+              <Link to="/forgot-password" className="text-sm text-blue-400 hover:underline">
+                Forgot password?
+              </Link>
+            </div>
+            {error && <p className="text-red-500 font-semibold mb-2">{error}</p>}
+
+            <motion.button
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              className="w-full py-3 px-4 bg-gradient-to-r from-blue-700 to-custom-blue text-white font-bold rounded-lg shadow-lg hover:from-blue-600 hover:to-custom-blue focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? <Loader className="w-6 h-6 animate-spin mx-auto" /> : "Login"}
+            </motion.button>
+          </form>
+        </div>
+        <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+          <p className="text-sm text-gray-400">
+            Don&apos;t have an account?{" "}
+            <Link to="/operator-signup" className="text-blue-400 hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default OperatorLoginPage;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -28,20 +28,19 @@ const SignUpPage = () => {
 
 	// Function to format phone number
 	const formatPhoneNumber = (value) => {
-		// Remove all non-digit characters
-		const digits = value.replace(/\D/g, '');
-		let formatted = '';
+		const digits = value.replace(/\D/g, "");
+		let formatted = "";
 
 		if (digits.length > 0) {
 			formatted += digits.slice(0, 3); // First 3 digits
 		}
 		if (digits.length > 3) {
-			formatted += ' - ' + digits.slice(3, 6); // Next 3 digits
+			formatted += " - " + digits.slice(3, 6); // Next 3 digits
 		}
 		if (digits.length > 6) {
-			formatted += ' - ' + digits.slice(6, 10); // Last 4 digits
+			formatted += " - " + digits.slice(6, 10); // Last 4 digits
 		}
-		return formatted.trim(); // Remove trailing spaces
+		return formatted.trim();
 	};
 
 	const handleNumberChange = (e) => {
@@ -51,91 +50,99 @@ const SignUpPage = () => {
 	};
 
 	const handleKeyDown = (e) => {
-		// Allow Backspace and Delete keys for editing
-		if (e.key === 'Backspace' || e.key === 'Delete') {
+		if (e.key === "Backspace" || e.key === "Delete") {
 			e.preventDefault();
 			const cursorPosition = e.target.selectionStart;
-			const digits = number.replace(/\D/g, ''); // Get current digits only
+			const digits = number.replace(/\D/g, "");
 
 			if (cursorPosition > 0) {
 				const newDigits = digits.slice(0, cursorPosition - 1) + digits.slice(cursorPosition);
-				setNumber(formatPhoneNumber(newDigits)); // Update state with new format
+				setNumber(formatPhoneNumber(newDigits));
 			}
 		}
 	};
 
 	return (
-		<motion.div
-			initial={{ opacity: 0, y: 20 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden mx-4 sm:mx-auto' // Added mx-4 for mobile padding
+		<div
+			className='min-h-screen w-full flex items-center justify-center'
+			style={{
+				backgroundImage: "linear-gradient(135deg, #1B2E52 0%, #5185AB 100%)",
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+			}}
 		>
-			<div className='p-6'> {/* Reduced padding */}
-				<h2 className='text-2xl font-bold mb-4 text-center bg-gradient-to-r from-blue-900 to-blue-600 text-transparent bg-clip-text'>
-					Create Account
-				</h2>
+			<motion.div
+				initial={{ opacity: 0, y: 20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+				className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden mx-4 sm:mx-auto'
+			>
+				<div className='p-6'>
+					<h2 className='text-2xl font-bold mb-4 text-center bg-gradient-to-r from-blue-900 to-blue-600 text-transparent bg-clip-text'>
+						Create Account
+					</h2>
 
-				<form onSubmit={handleSignUp}>
-					<Input
-						icon={User}
-						type='text'
-						placeholder='Full Name'
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						className='text-sm' // Adjusted text size for input
-					/>
-					<Input
-						icon={Mail}
-						type='email'
-						placeholder='Email Address'
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						className='text-sm'
-					/>
-					<Input
-						icon={Lock}
-						type='password'
-						placeholder='Password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						className='text-sm'
-					/>
-					<Input
-						icon={Phone} // Use Phone icon
-						type='text' // Specify type as 'text' for better control
-						placeholder='Phone Number'
-						value={number}
-						onChange={handleNumberChange} // Call the new handler for number formatting
-						onKeyDown={handleKeyDown} // Call the key down handler for backspace/delete
-						className='text-sm'
-					/>
-					{error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
-					<PasswordStrengthMeter password={password} />
+					<form onSubmit={handleSignUp}>
+						<Input
+							icon={User}
+							type='text'
+							placeholder='Full Name'
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							className='text-sm'
+						/>
+						<Input
+							icon={Mail}
+							type='email'
+							placeholder='Email Address'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className='text-sm'
+						/>
+						<Input
+							icon={Lock}
+							type='password'
+							placeholder='Password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							className='text-sm'
+						/>
+						<Input
+							icon={Phone}
+							type='text'
+							placeholder='Phone Number'
+							value={number}
+							onChange={handleNumberChange}
+							onKeyDown={handleKeyDown}
+							className='text-sm'
+						/>
+						{error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
+						<PasswordStrengthMeter password={password} />
 
-					<motion.button
-						className='mt-4 w-full py-2 px-3 bg-gradient-to-r from-custom-blue to-blue-600 text-white 
+						<motion.button
+							className='mt-4 w-full py-2 px-3 bg-gradient-to-r from-custom-blue to-blue-600 text-white 
 						font-bold rounded-lg shadow-lg hover:from-custom-blue
 						hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
 						focus:ring-offset-gray-900 transition duration-200'
-						whileHover={{ scale: 1.02 }}
-						whileTap={{ scale: 0.98 }}
-						type='submit'
-						disabled={isLoading}
-					>
-						{isLoading ? <Loader className='animate-spin mx-auto' size={20} /> : "Sign Up"} {/* Reduced size */}
-					</motion.button>
-				</form>
-			</div>
-			<div className='px-6 py-3 bg-gray-900 bg-opacity-50 flex justify-center'> {/* Adjusted padding */}
-				<p className='text-xs text-gray-400'> {/* Reduced font size */}
-					Already have an account?{" "}
-					<Link to={"/login"} className='text-blue-600 hover:underline'>
-						Login
-					</Link>
-				</p>
-			</div>
-		</motion.div>
+							whileHover={{ scale: 1.02 }}
+							whileTap={{ scale: 0.98 }}
+							type='submit'
+							disabled={isLoading}
+						>
+							{isLoading ? <Loader className='animate-spin mx-auto' size={20} /> : "Sign Up"}
+						</motion.button>
+					</form>
+				</div>
+				<div className='px-6 py-3 bg-gray-900 bg-opacity-50 flex justify-center'>
+					<p className='text-xs text-gray-400'>
+						Already have an account?{" "}
+						<Link to={"/login"} className='text-blue-600 hover:underline'>
+							Login
+						</Link>
+					</p>
+				</div>
+			</motion.div>
+		</div>
 	);
 };
 
